bd8f8a9f82e021bedb456d3d57911604b3389ab1
anthony-rio <95897978+anthony-rio@users.noreply.github.com>
2025-08-01 11:54:32 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="football",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "supervision",
+        "opencv-python",
+        "numpy"
+    ],
+    author="anthony-rio",
+    description="Football field analysis and visualization tools",
+    python_requires=">=3.7",
+)
